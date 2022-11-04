5f5b310fff4ebf783b6233f3493c41d3a10162ed
Oleg Gubskiy <gubskyolegod@gmail.com>
2022-11-04 18:51:03 +0200

    add task-2-1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,36 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Получаем ссылку на element  <ul id="ingredients"></ul>
+
+const listIngridients = document.querySelector("#ingredients");
+
+// createElementsItem перебирает массив данных и создает колекцию елементов  LI
+
+const createElementsItem = (array) => {
+  const itemsElement = array.reduce((acc, item) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = item;
+    liEl.classList.add("item");
+    acc.push(liEl);
+    return acc;
+  }, []);
+  return itemsElement;
+};
+
+console.log(createElementsItem(ingredients));
+
+//Функция renderItemList рендерит колекцию елементов
+
+const renderItemList = (list, items) => {
+  list.append(...items);
+};
+
+const liElements = createElementsItem(ingredients);
+renderItemList(listIngridients, liElements);
