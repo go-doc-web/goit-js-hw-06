d7638bc5c1493a8998b77d68a0ed93e246e46c21
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2022-11-04 07:48:42 +0200

    add task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,23 @@
+//count the number of categories
+
+const categoriesEl = document.getElementById("categories");
+const itemEl = document.querySelectorAll(".item");
+
+console.log(itemEl);
+
+const getAmountCategories = () => {
+  const numbersOfCategories = categoriesEl.querySelectorAll(".item").length;
+  console.log(`Number of categories: ${numbersOfCategories}`);
+};
+
+const getTitleItemAndAmountOfCategories = () => {
+  itemEl.forEach((item) => {
+    const titleItem = item.querySelector("h2");
+    const amountEl = item.querySelectorAll(".item ul li").length;
+    console.log(`Category: ${titleItem.textContent}`);
+    console.log(`Elements: ${amountEl}`);
+  });
+};
+
+getAmountCategories();
+getTitleItemAndAmountOfCategories();
