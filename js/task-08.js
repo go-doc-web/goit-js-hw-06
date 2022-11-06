483ee195466271d4329f41e184878f2e5506e349
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2022-11-06 15:02:11 +0200

    add task-8-1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,35 @@
+/*
+Обработка отправки формы form.login-form должна быть по событию submit.
+При отправке формы страница не должна перезагружаться.
+Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset. 
 
+*/
+
+const refs = {
+  loginForm: document.querySelector(".login-form"),
+};
+// console.log(refs.loginForm);
+
+refs.loginForm.addEventListener("submit", onLoginFormSubmit);
+
+function onLoginFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("! Заполнены не все поля формы");
+  }
+
+  const dataForm = {
+    email: email.value,
+    password: password.value,
+  };
+  //   console.log(dataForm);
+  e.currentTarget.reset();
+
+  console.log(dataForm);
+}
